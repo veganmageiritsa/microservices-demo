d5c18226b5c1b8a33b4b75ce43240aec1fe92998
nikolas leontiou <nle@anixe.io>
2023-03-03 14:47:49 +0200

    Elastic Query Service

---
+++ elastic-query-service/src/test/java/com/microservices/demo/elastic/query/service/ElasticQueryServiceApplicationTest.java
@@ -0,0 +1,17 @@
+package com.microservices.demo.elastic.query.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class ElasticQueryServiceApplicationTest {
+    
+    @Test
+    void contextLoads(){
+    
+    }
+}
