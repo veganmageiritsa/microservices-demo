912f0628e85cb8d3e84ee324257bd3af66e75e82
nikolas leontiou <nle@anixe.io>
2023-03-04 19:14:01 +0200

    Versioning By Uri

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -2,6 +2,8 @@ package com.microservices.demo.elastic.query.service.api;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -19,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import com.microservices.demo.elastic.query.service.business.ElasticQueryService;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModelV2;
 
 @RestController
 @RequestMapping(value = "/documents")
@@ -32,19 +35,58 @@ public class ElasticDocumentController {
         this.elasticQueryService = elasticQueryService;
     }
     
-    @GetMapping("/")
+    @GetMapping("/v1")
     public ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments() {
         return ResponseEntity.ok(elasticQueryService.getAllDocuments());
     }
     
-    @GetMapping("/{id}")
+    @GetMapping("v1/{id}")
     public ResponseEntity<ElasticQueryServiceResponseModel> getDocumentById(@PathVariable @NotEmpty String id) {
         return ResponseEntity.ok(elasticQueryService.getDocumentById(id));
     }
     
-    @PostMapping("/getDocumentByText")
+    @PostMapping("v1//getDocumentByText")
     public ResponseEntity<List<ElasticQueryServiceResponseModel>> getDocumentByText(@RequestBody @Valid ElasticQueryServiceRequestModel requestModel) {
         return ResponseEntity.ok(elasticQueryService.getDocumentByText(requestModel.getText()));
     }
     
+    @GetMapping("/v2")
+    public ResponseEntity<List<ElasticQueryServiceResponseModelV2>> getAllDocumentsV2() {
+        List<ElasticQueryServiceResponseModelV2> elasticQueryServiceResponseModelV2s =
+            elasticQueryService.getAllDocuments()
+                               .stream()
+                               .map(convertFunction)
+                               .collect(Collectors.toList());
+        return ResponseEntity.ok(elasticQueryServiceResponseModelV2s);
+    }
+    
+    
+    @GetMapping("v2/{id}")
+    public ResponseEntity<ElasticQueryServiceResponseModelV2> getDocumentByIdV2(@PathVariable @NotEmpty String id) {
+        return ResponseEntity.ok(convertFunction.apply(elasticQueryService.getDocumentById(id)));
+    }
+    
+    @PostMapping("v2//getDocumentByText")
+    public ResponseEntity<List<ElasticQueryServiceResponseModelV2>> getDocumentByTextV2(@RequestBody @Valid ElasticQueryServiceRequestModel requestModel) {
+        List<ElasticQueryServiceResponseModelV2> elasticQueryServiceResponseModelV2s =
+            elasticQueryService.getDocumentByText(requestModel.getText())
+                               .stream()
+                               .map(convertFunction)
+                               .collect(Collectors.toList());
+    
+        return ResponseEntity.ok(elasticQueryServiceResponseModelV2s);
+    
+    }
+    
+    
+    Function<ElasticQueryServiceResponseModel, ElasticQueryServiceResponseModelV2> convertFunction =
+        response -> ElasticQueryServiceResponseModelV2
+            .builder()
+            .userId(response.getUserId())
+            .text(response.getText())
+            .id(String.valueOf(response.getId()))
+            .createdAt(response.getCreatedAt())
+            .build().add(response.getLinks());
+    
 }
+
---
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/model/ElasticQueryServiceResponseModelV2.java
@@ -0,0 +1,22 @@
+package com.microservices.demo.elastic.query.service.model;
+
+import java.time.ZonedDateTime;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ElasticQueryServiceResponseModelV2 extends RepresentationModel<ElasticQueryServiceResponseModelV2> {
+    private String id;
+    private Long userId;
+    private String text;
+    
+    private ZonedDateTime createdAt;
+}
